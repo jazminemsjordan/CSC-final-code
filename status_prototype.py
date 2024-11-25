204598aef58fc9dcca2430ceee5a78a760cab0ce
Jazmine Jordan <jazminejordan77@gmail.com>
2024-11-24 22:14:22 -0500

    Create status_prototype.py

---
+++ status_prototype.py
@@ -0,0 +1,8 @@
+def initialize():
+  ''' globalizes necessary variables, sets them to a default state for a new game'''
+
+def end_menu():
+    """ Shows end data and stats, variable counter for turns taken added every time a command is successful."""
+
+def start_menu():
+    """ Call instructions function and ask whether to begin a new game or reset."""
