af2351ebb5abff7889a497fcf887f8f07d3ba785
Jazmine Jordan <jazminejordan77@gmail.com>
2024-12-02 08:34:16 -0800

    Update status_module.py

--- status_module.py
+++ status_module.py
@@ -4,7 +4,7 @@ def initialize():
     global locked
     locked = {"supply_closet": True, "classroom_2": True, "classroom_3": True, "dean_office": True}
     global item_pairs
-    item_pairs = {"old key": "old door", "keycard": "keycard door", "rusty key": "rusty door", "shiny key": "shiny door", "gold key": "gold door", "small key": "small drawer"}
+    item_pairs = {"old key": "old door", "keycard": "keycard door", "rusty key": "rusty door", "shiny key": "shiny door", "gold key": "gold door", "drawer key": "librarian's desk"}
     global inventory
     inventory = []
     global turn
@@ -19,10 +19,10 @@ def start_menu():
     """ Call instructions function and ask whether to begin a new game or reset."""
 
 vestibule_flavor = "text"
-vestibule_objects = []
+vestibule_objects = ["keycard door"]
 
 atrium_flavor = "text"
-atrium_objects = []
+atrium_objects = ["old door", "rusty door"]
 
 classroom_1_flavor = ""
 classroom_1_objects = []
@@ -37,7 +37,7 @@ supply_closet_flavor = ""
 supply_closet_objects = []
 
 library_flavor = "You are in the library. Floor to ceiling bookshelves line the walls, with a few cozy looking chairs and couches in the center. Objects of interest: librarian's desk, study table, mahogany bookcase. To the north is Classroom 3. To the east is the Dean's Office. To the South is the balcony. To the West is the stairwell down to the atrium."
-library_objects = ["librarian's desk", "study table", "mahogany bookcase"]
+library_objects = ["librarian's desk", "study table", "mahogany bookcase", "shiny door", "gold door"]
 
 dean_office_flavor = ""
 dean_office_objects = []
