e5291e10f55695db9c1a16da4171e8b44fe7a573
Evelyn-Dinh <ltdinh@smith.edu>
2024-12-04 12:00:32 -0500

    Change the command_module to status_module import

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import command_module
 import status_module
 
 def main():
-    command_module.start_menu()
+    status_module.start_menu()
     while True:
         command_module.get_input()
         status_module.turns += 1
         if status_module.win == True:
-            command_module.end_menu()
+            status_module.end_menu()
 
 
 
