2ba938c42e4d140bc588d4a4cde17192aef555b4
Jazmine Jordan <jazminejordan77@gmail.com>
2024-12-04 11:30:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import command_module
 import status_module
 
 def main():
-    command_module.menu()
+    command_module.start_menu()
     while True:
         command_module.get_input()
+        status_module.turns += 1
         if status_module.win == True:
             command_module.end_menu()
 
